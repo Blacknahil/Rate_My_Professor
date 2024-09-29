d1305396a127468c220d3b505515dfd57e8eb671
Nahom Garefo <harobina28@gmail.com>
2024-09-29 17:11:01 +0300

    added UI and interaction with the api/chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,108 @@
-import Image from "next/image";
+'use client'
+import { Box,Typography,Button,Stack,TextField } from "@mui/material"
+import { useState } from "react"
 
-export default function Home() {
+export default function Home(){
+
+  const [messages,setMessages] = useState([{
+    role:"assistant",
+    content:`Hi! I'm the Rate My Professor support assistant. How can I help you today?`
+  }])
+  const [message,setMessage]= useState('')
+
+
+
+
+  const sendMessage= async() =>{
+
+    setMessage('')
+    setMessages((messages)=>[
+      ...messages,
+      {role:'user', content:message},
+      {role:'assistant',content:''}
+    ])
+
+    const response = await fetch('api/chat',{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify([...messages,{role:'user',content:message}])
+
+    }).then(async (res)=>{
+      const decoder= new TextDecoder()
+      if (res.body){
+        const reader= res.body.getReader()
+        const  result=''
+
+        return reader.read().then(function processText({done, value}):Promise<string> {
+          if (done) {
+            return Promise.resolve(result)
+          }
+          const text = decoder.decode(value || new Uint8Array(), {stream: true})
+          setMessages((messages) => {
+            const  lastMessage = messages[messages.length - 1]
+            const  otherMessages = messages.slice(0, messages.length - 1)
+            return [
+              ...otherMessages,
+              {...lastMessage, content: lastMessage.content + text},
+            ]
+          })
+          return reader.read().then(processText)
+        })
+    
+  }
+})
+  }
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <Box
+    width="100vw"
+    height="100vh"
+    display="flex"
+    flexDirection="column"
+    justifyContent="center"
+    alignItems="center"
+    >
+      <Stack
+            direction={'column'}
+            width="500px"
+            height="700px"
+            border="1px solid black"
+            p={2}
+            spacing={3}
+      >
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Stack 
+        direction={'column'}
+        spacing={2}
+        flexGrow={1}
+        overflow='auto'
+        maxHeight='100%'
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        {messages.map((message,index)=>(
+          <Box key={index} display="flex" 
+          justifyContent={ message.role=="assistant" ? 'flex-start':'flex-end'}>
+            <Box bgcolor={ message.role=="assistant" ? 'primary.main': 'secondary.main'}
+            p={2}
+            
+            >{message.content}</Box>
+
+          </Box>
+        ))}
+        </Stack>
+        <Stack>
+          <TextField
+          label="Message"
+          fullWidth 
+          value={message}
+          onChange={(e)=>{setMessage(e.target.value)}}
+           />
+          <Button variant="contained" onClick={sendMessage}>Send</Button>
+        </Stack>
+      </Stack>
+    </Box>
+  )
+
 }
